fb3f22b8d958ccdc0382c6921c3a4b7f03cbf209
jeffrepo <jeffer.leo93@gmail.com>
2024-08-14 16:13:26 -0600

    Update 15.0

--- __manifest__.py
+++ __manifest__.py
@@ -13,10 +13,10 @@
     'depends': ['hr','hr_contract','hr_payroll'],
     'data': [
         'views/hr_views.xml',
-        'views/report.xml',
-        'views/hr_gt_views.xml',
+        # 'views/report.xml',
+        # 'views/hr_gt_views.xml',
         'data/data.xml',
-        'views/hr_contract_views.xml',
+        # 'views/hr_contract_views.xml',
         'views/res_company_views.xml',
         'security/ir.model.access.csv',
     ],
